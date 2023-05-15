351ff7985b471387fddaeb0cc10b55f3cad13fe8
Isaura Martí <imarti.teixido@gmail.com>
2023-05-15 08:30:43 +0200

    feat create routes login and home pages

---
+++ src/routes/routes.jsx
@@ -0,0 +1,24 @@
+import { Suspense, lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+const Login = lazy(() => import("./../views/pages/Login"));
+const Home = lazy(() => import("./../views/pages/Home"));
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: (
+      <Suspense fallback={<></>}>
+        <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<></>}>
+        <Home />
+      </Suspense>
+    ),
+  },
+]);
